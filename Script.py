9a2ffa401de27f7ade903bfd60b9d090ddfa997f
Russell <RustyReich@gmail.com>
2023-05-05 17:24:36 -0700

    Create DataFrame and add File name column

--- Script.py
+++ Script.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 DATASET_PATH = "Dataset"
 
@@ -9,11 +10,18 @@ ActorFolders = os.listdir(DATASET_PATH)
 for FolderName in ActorFolders:
 
     ActorPath = DATASET_PATH + "/" + FolderName
-
     WavFileNames = os.listdir(ActorPath)
 
     for WavFileName in WavFileNames:
 
         WavFilePath = ActorPath + "/" + WavFileName
-
         WavFilePaths.append(WavFilePath)
+
+Data = {'File Name': []}
+
+for WavFilePath in WavFilePaths:
+
+    FileName = WavFilePath[(WavFilePath.rindex("/") + 1):]
+    Data['File Name'].append(FileName)
+
+Frame = pd.DataFrame(data = Data)
